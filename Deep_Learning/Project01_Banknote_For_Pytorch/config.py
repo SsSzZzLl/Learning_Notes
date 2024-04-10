37cc8152008df7fabc2f61216f7463c2e8bff2ce
shalebaji <zhuolisong102980@163.com>
2024-04-10 11:39:34 +0800

    update

--- Deep_Learning/Project01_Banknote_For_Pytorch/config.py
+++ Deep_Learning/Project01_Banknote_For_Pytorch/config.py
@@ -2,16 +2,53 @@
 Author: Szl
 Date: 2024-04-10 10:33:54
 LastEditors: Szl
-LastEditTime: 2024-04-10 10:37:10
-Description: 
+LastEditTime: 2024-04-10 11:02:24
+Description: 编写config配置文件，配置所需的超参
 '''
 
 # Package and Modules import statements
 # -----------------------
 
-
 # codings
 # -----------------------
 
+class Hyperparameter(object):
+
+  '''
+    超参数配置对象
+  '''
+
+  # 第一部分参数：data - 数据集相关的超参数配置
+  
+  device = 'cude' # 该参数表示所需的硬件算力类型，取值为cuda还是cpu
+  data_dir = './data' # 数据集的根目录路径
+  data_path = './data/data_banknote_authentication.txt' # 指定原始数据集的路径
+  
+  trainset_path = './data/train.txt' # 划分完毕的训练集数据
+  devset_path = './data/dev.txt' # 划分完毕的验证集数据
+  testset_path = './data/test.txt' # 划分完毕的测试集数据
+  
+  in_features = 4 # 输入特征维度，特征的数量n
+  out_dims = 2 # 输出特征维度，最终输出结果的所有可能的类别
+  seed = 1234 # 固定的随机种子
+  
+  # 第二部分参数：demol structure - 模型结构相关的超参数配置
+  
+  layer_list = [in_features, 64, 128, 64, out_dims] # 使用一个list组织整个网络的结构以及各个神经网络层的输入和输出维度
+
+  # 第三部分参数：training - 训练相关的超参数配置
+    
+  batch_size = 64 # 一次训练时输入的数据样本个数 - 直接决定模型的收敛速度，其次会直接影响模型训练的效率，所以超参在真正的工程化开发中需要调参（过小会导致模型收敛效率较低，过大会导致现存溢出，终端训练） - 但目前该值给定一个小于128大于32的值即可
+  
+  init_lr = 0.001 # 初始学习率Learning_rate
+  epoches = 100 # 训练的轮数
+  verbose_step = 10 # 设置每训练完成10次打印一次训练输出
+  save_step = 200 # 每训练200步进行一次模型保存
+  
+HP = Hyperparameter()
+
 # run test UseCase if current modules in main
 # -----------------------
+
+if __name__ == '__main__':
+  pass
--- Deep_Learning/Project01_Banknote_For_Pytorch/preprocessing.py
+++ Deep_Learning/Project01_Banknote_For_Pytorch/preprocessing.py
@@ -2,16 +2,66 @@
 Author: Szl
 Date: 2024-04-10 10:32:42
 LastEditors: Szl
-LastEditTime: 2024-04-10 10:37:27
-Description: 
+LastEditTime: 2024-04-10 11:20:35
+Description: 实现数据预处理脚本，同时完成数据集划分
+正常的工程化发开场景中，该脚本往往只需要单独运行，所以是否采用函数或对象封装都可以
 '''
 
 # Package and Modules import statements
 # -----------------------
 
+import numpy as np
+from config import HP
+import os
 
 # codings
 # -----------------------
 
+def data_split(data_path):
+  
+  '''
+    :param data_path: 原始数据集的路径
+    :return: 
+  '''
+
+  # 设置训练集、验证集、测试集划分比例为7：2：1
+  trainset_ratio = 0.7
+  devset_ratio = 0.2
+  testset_ratio = 0.1
+  
+  # 设置随机种子
+  np.random.seed(HP.seed)
+
+  # 从给定的原始数据集路径加载原始数据集
+  dataset = np.loadtxt(data_path, delimiter= ',')
+  
+  # 进行数据集的随机重新排列
+  np.random.shuffle(dataset)
+  
+  # 获取样本数量
+  n_items = dataset.shape[0]
+  
+  # 指定训练集样本数量
+  trainset_num = int(trainset_ratio * n_items)
+  
+  # 指定验证集样本数量
+  devset_num = int(devset_ratio * n_items)
+  
+  # 指定测试集样本数量
+  testset_num = n_items - trainset_num - devset_num
+  
+  # 划分并生成训练集数据
+  np.savetxt(os.path.join(HP.data_dir, 'train.txt'), dataset[: trainset_num], delimiter = ',')
+  
+  # 划分并生成验证集数据
+  np.savetxt(os.path.join(HP.data_dir, 'dev.txt'), dataset[trainset_num : trainset_num + devset_num], delimiter = ',')
+  
+  # 划分并生成测试集数据
+  np.savetxt(os.path.join(HP.data_dir, 'test.txt'), dataset[trainset_num + devset_num :], delimiter = ',')
+  
+
 # run test UseCase if current modules in main
 # -----------------------
+
+if __name__ == '__main__':
+  data_split(HP.data_path)
